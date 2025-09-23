ef83139f8f24a945e40183401bb862a75fa5acab
Oğuzhan "Ozzy" Akçalı <oguzkhanakcali@gmail.com>
2025-09-23 16:01:24 +0300

    Update page.tsx

--- app/[locale]/admin/user-equity/page.tsx
+++ app/[locale]/admin/user-equity/page.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react'
 import { UserEquityDashboard } from '@/app/[locale]/admin/components/user-equity/user-equity-dashboard'
 
+export const dynamic = 'force-dynamic';
 export default function UserEquityPage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
